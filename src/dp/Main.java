821b8f662ca4a86362d5ff797ac64eee15c5d6c0
bigth21 <taekhyeon.nam@gmail.com>
2025-04-22 18:06:45 +0900

    Polishing

--- src/dp/Main.java
+++ src/dp/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) {
         List<Fibonacci> fibonacciList = List.of(new BruteForce(), new Tabulation(), new Memoization());
         for (Fibonacci fibonacci : fibonacciList) {
-            long startTime = System.currentTimeMillis();
+            long startTime = System.nanoTime();
 
             System.out.println(fibonacci.getClass().getSimpleName() + ".execute() = " + fibonacci.calculate(45));
 
-            long endTime = System.currentTimeMillis();
-            System.out.println("Time taken: " + (endTime - startTime) + " ms");
+            long endTime = System.nanoTime();
+            System.out.println("Time taken: " + (endTime - startTime) + " ns");
         }
     }
 }
--- src/sieveoferasthenes/Main.java
+++ src/sieveoferasthenes/Main.java
@@ -6,14 +6,14 @@ public class Main {
     public static void main(String[] args) {
         List<PrimeNumbers> primeNumbers = List.of(new BruteForce(), new SieveOfEratosthenes());
         for (PrimeNumbers primeNumber : primeNumbers) {
-            long startTime = System.currentTimeMillis();
+            long startTime = System.nanoTime();
 
             int n = 50;
             List<Integer> primes = primeNumber.findPrimes(n);
             System.out.println("primes = " + primes);
 
-            long endTime = System.currentTimeMillis();
-            System.out.println("Time taken: " + (endTime - startTime) + " ms");
+            long endTime = System.nanoTime();
+            System.out.println("Time taken: " + (endTime - startTime) + " ns");
         }
     }
 }
