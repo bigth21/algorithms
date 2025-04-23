79fc71c5faef9f3a35182c7db464d8f95c27f8f7
bigth21 <taekhyeon.nam@gmail.com>
2025-04-23 23:07:36 +0900

    Add brute force implementation of finding prime numbers

--- src/sieveoferasthenes/BruteForce.java
+++ src/sieveoferasthenes/BruteForce.java
@@ -1,10 +1,25 @@
 package sieveoferasthenes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BruteForce implements PrimeNumbers {
     @Override
     public List<Integer> findPrimes(int n) {
-        return List.of();
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i))
+                primes.add(i);
+        }
+        return primes;
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
